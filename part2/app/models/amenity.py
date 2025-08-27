b9c3d2d205233486b78ed196bca98b2b1b6895fb
vorw <sultanalsanie23@gmail.com>
2025-08-27 15:01:14 +0000

    Amenity Class

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+
+from app.models.BaseModel import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity Class"""
+
+    def __init__(self, name):
+        """init"""
+        super().__init__()
+        
+        if not name or len(name) > 50:
+            raise ValueError("Please make sure its 50 characters or less.")
+        
+        self.name = name
