5a4e2247a5fbf177e2333bdda605171344b1fe97
vorw <sultanalsanie23@gmail.com>
2025-09-16 16:15:35 +0000

    task0

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -3,11 +3,13 @@
 
 from flask import Flask
 from flask_restx import Api
+from config import config
 
-def create_app():
+def create_app(cname="default"):
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
+    app.config.from_object(config[cname])
     # Import and register API namespaces
     from app.api.v1.users import api as users_api
     from app.api.v1.amenities import api as amenities_api
