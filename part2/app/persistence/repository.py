07078e580ec57878f665f209629c36c3cef555e1
vorw <sultanalsanie23@gmail.com>
2025-09-20 13:22:06 +0000

    Tasks

--- part2/app/persistence/repository.py
+++ part2/app/persistence/repository.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
-
 from abc import ABC, abstractmethod
+from app import db
 
 class Repository(ABC):
     @abstractmethod
-    def add(self, obj):
+    def add(self, instance):
         pass
 
     @abstractmethod
-    def get(self, obj_id):
+    def get(self, instance_id):
         pass
 
     @abstractmethod
@@ -17,39 +17,70 @@ class Repository(ABC):
         pass
 
     @abstractmethod
-    def update(self, obj_id, data):
+    def update(self, instance_id, data):
         pass
 
     @abstractmethod
-    def delete(self, obj_id):
+    def delete(self, instance_id):
         pass
 
     @abstractmethod
-    def get_by_attribute(self, attr_name, attr_value):
+    def get_by_attribute(self, attribute_name, attribute_value):
         pass
-
 
 class InMemoryRepository(Repository):
     def __init__(self):
-        self._storage = {}
+        self.storage = {}
 
-    def add(self, obj):
-        self._storage[obj.id] = obj
+    def add(self, instance):
+        self.storage[instance.id] = instance
 
-    def get(self, obj_id):
-        return self._storage.get(obj_id)
+    def get(self, instance_id):
+        return self.storage.get(instance_id)
 
     def get_all(self):
-        return list(self._storage.values())
+        return list(self.storage.values())
 
-    def update(self, obj_id, data):
-        obj = self.get(obj_id)
-        if obj:
-            obj.update(data)
+    def update(self, instance_id, data):
+        instance = self.get(instance_id)
+        if instance:
+            instance.update(data)
 
-    def delete(self, obj_id):
-        if obj_id in self._storage:
-            del self._storage[obj_id]
+    def delete(self, instance_id):
+        if instance_id in self.storage:
+            del self.storage[instance_id]
 
-    def get_by_attribute(self, attr_name, attr_value):
-        return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
+    def get_by_attribute(self, attribute_name, attribute_value):
+        return next((instance for instance in self.storage.values() if getattr(instance, attribute_name) == attribute_value), None)
+
+class SQLAlchemyRepository(Repository):
+    def __init__(self, model):
+        self.model = model
+
+    def add(self, instance):
+        db.session.add(instance)
+        db.session.commit()
+
+    def get(self, instance_id):
+        return self.model.query.get(instance_id)
+
+    def get_all(self):
+        return self.model.query.all()
+
+    def update(self, instance_id, data):
+        instance = self.get(instance_id)
+        if instance:
+            for key, value in data.items():
+                setattr(instance, key, value)
+            db.session.commit()
+            return instance
+        return None
+
+    def delete(self, instance_id):
+        instance = self.get(instance_id)
+        if instance:
+            db.session.delete(instance)
+            db.session.commit()
+
+    def get_by_attribute(self, attribute_name, attribute_value):
+        return self.model.query.filter_by(**{attribute_name: attribute_value}).first()
